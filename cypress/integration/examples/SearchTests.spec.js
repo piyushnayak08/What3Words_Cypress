5d97c9453f5554b4ac487a1de1c9588ebff19009
Piyush <piyushnayak08@gmail.com>
2021-07-09 22:51:52 +0100

    Search by 3 word address test case

--- cypress/integration/examples/SearchTests.spec.js
+++ cypress/integration/examples/SearchTests.spec.js
@@ -2,9 +2,10 @@ import SearchPage from '../pageObjects/SearchPage'
 
 describe('What3words search feature', () => {
 
-    it('Search for 3 word address', () => {
+    it('Search by 3 word address', () => {
         const sp = new SearchPage()
         sp.launchApp()
         sp.searchAddress('certified.potato.vine')
+        sp.selectDropdownThreeWords('certified.potato.vine')
     })
 })
--- cypress/integration/pageObjects/SearchPage.js
+++ cypress/integration/pageObjects/SearchPage.js
@@ -7,6 +7,30 @@ class SearchPage {
   searchAddress(value) {
     cy.get("[data-testid=ThreeWordAddress-Text]").click();
     cy.get("[data-testid=SearchPanel-Input]").type(value);
+    cy.wait(3000)
+  }
+  selectDropdownThreeWords(value) {
+    cy.get('.notranslate').each(($el, index, $list) => {
+        if ($el.text() === value) {
+          cy.wrap($el).click()
+          cy.wait(1000)
+        }
+      })
+      cy.get('.notranslate').should('have.text',value)
+  }
+  selectDropdownName(value) {
+    cy.get('.SearchPanel-Location').each(($el, index, $list) => {
+        if ($el.text() === value) {
+          cy.wrap($el).click()
+        } 
+      })
+  }
+  selectDropdownCoordinate(value) {
+    cy.get('.SearchPanel-Location').each(($el, index, $list) => {
+        if ($el.text() === value) {
+          cy.wrap($el).click()
+        } 
+      })
   }
 }
 export default SearchPage;
